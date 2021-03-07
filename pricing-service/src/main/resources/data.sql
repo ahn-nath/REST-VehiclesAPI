2d286595a6d12e7b6f86760b3bc6b7bb4bdc66a4
ahn-nath <67247850+ahn-nath@users.noreply.github.com>
2021-03-07 16:27:49 -0400

    Initial commit

---
+++ pricing-service/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO price (vehicle_id, currency, price) VALUES (1, 'USD', 150000);
+
---
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -0,0 +1,36 @@
+package com.udacity.pricing;
+
+import static org.junit.Assert.assertEquals;
+
+import org.springframework.http.HttpStatus;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.udacity.pricing.domain.price.Price;                    
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc 
+public class PricingServiceApplicationTests {
+	@LocalServerPort
+	private int port = 8080;
+
+	@Autowired
+	private TestRestTemplate restTemplate; 
+	
+	@Test
+	public void getPricingService(){
+		ResponseEntity<Price> response =
+				this.restTemplate.getForEntity("http://localhost:" + port + "/services/price?vehicleId=1", Price.class);
+		
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+	
+	}
+}
